f2698557e61e8145a149e5ba8dd8a68e9b9d1fe8
Jose Gabriel ibáñez galdeano <gabriel_galde@hotmail.com>
2022-07-29 14:36:29 +0200

    Terminamos con Typescript

--- OrdenClase/Typescript/Ejemplo2_funciones/js/funciones.js
+++ OrdenClase/Typescript/Ejemplo2_funciones/js/funciones.js
@@ -1,8 +1,37 @@
 function saludar(nombre) {
     console.log("Hola ".concat(nombre));
 }
+// saludar('gabri')
+// console.log(sumar(7, 10))
 function sumar(num1, num2) {
     return num1 + num2;
 }
-saludar('gabri');
-console.log(sumar(5, 10));
+//Función con parámetros opcionales
+function mostrar(nombre, apellido) {
+    if (apellido) {
+        return "Mi nombre es ".concat(nombre, " y mi apellido es ").concat(apellido);
+    }
+    return "Mi nombre es ".concat(nombre);
+}
+// console.log(mostrar('Gabriel', 'Galdeano'))
+//parámetros por defecto
+function datos(nombre, soltero) {
+    if (soltero === void 0) { soltero = true; }
+    if (soltero)
+        return "Nuestro amigo ".concat(nombre, " est\u00E1 SOLTER\u00CDSIMO");
+    return "Nuestro amigo ".concat(nombre, " NO EST\u00C1 SOLTERO");
+}
+// console.log(datos(`Gabri`))
+// console.log(datos(`Gabri`, false))
+// console.log(datos(`Gabri`, true))
+// console.log(datos(`Gabri`))
+// De esta forma podemos decir que hay un número VARIABLE de argumentos
+function concatenar() {
+    var datos = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        datos[_i] = arguments[_i];
+    }
+    return datos.join('-');
+}
+console.log(concatenar('hola'));
+console.log(concatenar('hola', 'adios', 'hastaluegoLucas'));
--- OrdenClase/Typescript/Ejemplo2_funciones/js/funciones.ts
+++ OrdenClase/Typescript/Ejemplo2_funciones/js/funciones.ts
@@ -1,10 +1,36 @@
 function saludar(nombre: string): void {
 	console.log(`Hola ${nombre}`)
 }
+// saludar('gabri')
+// console.log(sumar(7, 10))
 
 function sumar(num1: number, num2: number): number {
 	return num1 + num2
 }
 
-saludar('gabri')
-console.log(sumar(5, 10))
+//Función con parámetros opcionales
+function mostrar(nombre: string, apellido?: string): string {
+	if (apellido) {
+		return `Mi nombre es ${nombre} y mi apellido es ${apellido}`
+	}
+	return `Mi nombre es ${nombre}`
+}
+// console.log(mostrar('Gabriel', 'Galdeano'))
+
+//parámetros por defecto
+function datos(nombre: string, soltero: boolean = true): string {
+	if (soltero) return `Nuestro amigo ${nombre} está SOLTERÍSIMO`
+	return `Nuestro amigo ${nombre} NO ESTÁ SOLTERO`
+}
+// console.log(datos(`Gabri`))
+// console.log(datos(`Gabri`, false))
+// console.log(datos(`Gabri`, true))
+// console.log(datos(`Gabri`))
+
+// De esta forma podemos decir que hay un número VARIABLE de argumentos
+function concatenar(...datos: any[]): string {
+	return datos.join('-')
+}
+
+console.log(concatenar('hola'))
+console.log(concatenar('hola', 'adios', 'hastaluegoLucas'))
