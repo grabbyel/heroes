dc5cf0f2301354ed6e93ca13ff8f4465a3d4355f
grabbyel <101138621+grabbyel@users.noreply.github.com>
2022-08-08 09:30:39 +0200

    PutoMAC: empezamos el lunes

---
+++ OrdenClase/Angular/Ejemplo7-login/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAZqeUd3lOwO6-oW5JrgGJIsTvMNZQ8mZw',
+    authDomain: 'hiberus-gabriel.firebaseapp.com',
+    projectId: 'hiberus-gabriel',
+    storageBucket: 'hiberus-gabriel.appspot.com',
+    messagingSenderId: '206752660533',
+    appId: '1:206752660533:web:c0de0118231a6a4945457c',
+  },
+};
